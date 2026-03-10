f314e8de30aeb697998330683e86f6533eee73e0
ALopez90 <candiauralexalopez@gmail.com>
2026-03-10 15:02:32 -0500

    Added PortfolioManager.vue
    Added today's lessons.

---
+++ src/services/portfolioMondayClient.js
@@ -0,0 +1,7 @@
+/**
+ * "service layer" for the Portfolio Manager component.
+ * This file will handle the "talking" to monday.com for us
+ * - PortfolioManager.vue handles the screen and user interaction
+ * - This file handles talking to monday.com
+ * - Keeps us organized!
+ */
